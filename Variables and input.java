17800697936a7ba8669e7b340ce9cf9959b1866d
Aditya Vishal Srivastava <137143275+AyVee-code@users.noreply.github.com>
2023-08-13 16:22:21 +0530

    Add files via upload

---
+++ Variables and input.java
@@ -0,0 +1,61 @@
+/* THE SOLUTION CODE HAS BEEN COMMENTED AS 3 DIFFERENT JAVA CODES ARE WRITTEN IN SAME FILE,
+   THUS THEY GENERATE MASSIVE ERRORS IF NOT COMMENTED */
+
+
+// Question 1: In a program, input 3 numbers A, B, C. You have to output the average of these 3 numbers.
+
+/*
+
+import java.util.*;
+public class practice {
+    public static void main(String agr[]){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int average = ( A + B + C) / 3;
+        System.out.println("Average: " + average);
+        sc.close();        
+    }
+}
+
+*/
+
+// Question 2: In a program, input the side of a square. you have to output the area of the square.
+
+/*
+
+import java.util.*;
+public class practice{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int area = A*A;
+        System.out.println("Area: " + area);
+        sc.close();
+    }
+}
+
+*/
+
+
+// Question 3: Enter cost of 3 items from the user (usingfloat data type) -pencil, a pen, and eraser. you 
+// have to output the total cost of the items back to the user as their bill.
+
+/*
+
+import java.util.*;
+public class practice{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float total = pencil + pen + eraser;
+        System.out.println("Your bill: " + total);
+        sc.close();
+    }
+}
+
+*/
+
